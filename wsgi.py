b61061acb1fb98d3879e7eaf473e621f9fa39fb4
Rishabh2991 <bhardwajrishabh76@gmail.com>
2018-11-13 18:34:06 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,25 @@
-from flask import Flask
-application = Flask(__name__)
+from flask import Flask,request,jsonify
+import requests
+app = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/',methods=['POST'])
+def temperature():
 
-if __name__ == "__main__":
-    application.run()
+ req_data = request.get_json()
+ city_name = req_data['cityname']
+ #area_code = req_data['areacode']
+ r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city_name+',in&appid=d092db6ed0c1484a47477095f3aefb80')
+ json_oj = r.json()
+ temp = float(json_oj['main']['temp'])
+ city = json_oj['name']
+ temp_f = (temp - 273.15) * 1.8 + 32
+ temp_c = (temp_f - 32) * .55
+ return jsonify(temperature=temp_c,
+                 cityname = city)
+
+#@app.route('/')
+#def index():
+#	return 'Hello'
+
+if __name__ == '__main__':
+  app.run(debug=True)
