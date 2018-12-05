867ff95fb084f85841b937656644966f237830b2
Rishabh2991 <bhardwajrishabh76@gmail.com>
2018-12-05 14:00:11 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,8 +6,12 @@ application = Flask(__name__)
 def temperature():
 
  req_data = request.get_json()
- city_name = req_data['cityname']
+ #city_name = req_data['cityname']
  #area_code = req_data['areacode']
+ 
+ for val in req_data['entities']:
+  city_name = val['value']
+  
  r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city_name+',in&appid=d092db6ed0c1484a47477095f3aefb80')
  json_oj = r.json()
  temp = float(json_oj['main']['temp'])
